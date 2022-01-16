73271b7b4b6f708d414fe9a7ba13c3d4107072d5
liuyuanyu <liuyuanyu12138@gmail.com>
2022-01-16 22:00:53 +0800

    update input

---
+++ RealEngine/Application/Reply.h
@@ -0,0 +1,27 @@
+#pragma once 
+
+struct Reply
+{
+public:
+    Reply(bool InIsHandled):bIshandled(InIsHandled){}
+
+    bool IsHandled() const ;
+    static Reply Unhandled();
+    static Reply Handled();
+private:
+    bool bIshandled;
+};
+
+Reply Reply::Unhandled()
+{
+    return Reply(false);
+}
+Reply Reply::Handled()
+{
+    return Reply(true);
+}
+
+bool Reply::IsHandled() const 
+{
+    return bIshandled;
+}
